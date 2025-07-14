25b05f0a3666aaef7900250b7c4d663c3965397c
ntpotraz <60411602+ntpotraz@users.noreply.github.com>
2025-07-13 17:44:21 -0700

    Got the look of the header bar. Started on summary

---
+++ src/Summary.jsx
@@ -0,0 +1,12 @@
+import "./Summary.css";
+import nathan from "./assets/nathan.jpg"
+
+const Summary = () => {
+  return (
+    <div className="summary">
+      <img className="nathan" src={nathan} alt="Me!" />
+    </div>
+  )
+}
+
+export default Summary
